0d76f9d312cdac44479e356654073fa3004b0fb5
Chan HY <chyng2000@gmail.com>
2023-02-27 07:18:25 +0000

    My XXXXX change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is awesome- edited");
+        System.out.println("Jenkins is awesome- edited2");
     }
 
     public String getMessage() {
